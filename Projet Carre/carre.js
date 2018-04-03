084cb1e69e9dad0c7f04ff244ba92e3c15ee78e5
Martin Dazin <martindazin@users.noreply.github.com>
2018-04-03 22:26:24 +0200

    Update carre.js

--- Projet Carre/carre.js
+++ Projet Carre/carre.js
@@ -1,13 +1,24 @@
+// Generate the table
+function generateTable(table, numberOfColumns, numberOfLines) {
+    for (let i = 0; i < numberOfLines; i++) {
+        table[i] = new Array();
+        for (let j = 0; j < numberOfColumns; j++) {
+            table[i][j] = getRandomInteger(10);
+        }
+    }
+    // Define a new Array() for methods :
+    // function sumColumn(myTable, numberOfColumns, numberOfLines), sumDiagonal(myTable, numberOfColumns, numberOfLines)
+    table[numberOfLines] = new Array();
+}
+
 // Generate a random integer
 function getRandomInteger(monNombreMaximum) {
     return Math.floor(Math.random() * Math.floor(monNombreMaximum));
 }
 
 // Sum of every boxes in a column
-// Add this sum in [numberOfLines + 1][k]
-// index is the number of the column
+// Add this sum in [numberOfLines][indexOfColumn]
 function sumColumn(table, numberOfColumns, numberOfLines) {
-    // table[numberOfLines] = new Array();
     for (let i = 0; i < numberOfColumns; i++) { 
         let sum = 0;
         for (let j = 0; j < numberOfLines; j++) {
@@ -23,8 +34,13 @@ function sumColumn(table, numberOfColumns, numberOfLines) {
     }
 }
 
-// Sum of the diagonale [0][0] to [numberOfColumns - 1][numberOfLines - 1] if numberOfLines === numberOfColumns
-// Add this sum in [numberOfLines][numberOfColumns]
+// if (numberOfLines === numberOfColumns) {
+    // Sum of the diagonale [0][0] to [numberOfColumns - 1][numberOfLines - 1] 
+    // Add this sum in [numberOfLines][numberOfColumns]
+// }
+// else {
+    // return null;   
+// }
 function sumDiagonal(table, numberOfColumns, numberOfLines) {
     if (numberOfColumns === numberOfLines) {
         let sum = 0;
@@ -45,7 +61,7 @@ function sumDiagonal(table, numberOfColumns, numberOfLines) {
 }
 
 // Sum of every boxes in a line
-// Add this sum in [i][numberOfColumns + 1]
+// Add this sum in [indexOfLine][numberOfColumns]
 function sumLine(table, numberOfColumns, numberOfLines) {
     for (let i = 0; i < numberOfLines; i++) { 
         let sum = 0;
@@ -58,7 +74,8 @@ function sumLine(table, numberOfColumns, numberOfLines) {
     }
 }
 
-// Sum of boxes
+// Sum of every boxes in the table
+// Return the variable sum which is a integer
 function sumOfTable(table, numberOfColumns, numberOfLines) {
     let sum = 0;
     for (let i = 0; i < numberOfLines; i++) {
@@ -69,31 +86,20 @@ function sumOfTable(table, numberOfColumns, numberOfLines) {
     return sum;
 }
 
-// Generate the table
-function generateTable(table, numberOfColumns, numberOfLines) {
-    for (let i = 0; i < numberOfLines; i++) {
-        table[i] = new Array();
-        for (let j = 0; j < numberOfColumns; j++) {
-            table[i][j] = getRandomInteger(10);
-        }
-    }
-    // Define a new Array() for methods :
-    // function sumColumn(table, index), sumDiagonal(table)
-    table[numberOfLines] = new Array();
-}
-
 // Call this function in console.log
 // and the other functions do the rest
+// Don't change the content of this function !
 function displayFinalTable() {
     let myTable = new Array();
     // Define the number of boxes
-    const numberOfColumns = 3; 
-    const numberOfLines = 7;
+    const numberOfColumns = 5; 
+    const numberOfLines = 5;
     // Execute functions
     generateTable(myTable, numberOfColumns, numberOfLines);
     sumColumn(myTable, numberOfColumns, numberOfLines);
-    sumLine(myTable, numberOfColumns, numberOfLines);
     sumDiagonal(myTable, numberOfColumns, numberOfLines);
+    sumLine(myTable, numberOfColumns, numberOfLines);
+    sumOfTable(myTable, numberOfColumns, numberOfLines)
 
 
     return myTable;
